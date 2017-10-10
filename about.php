324aa37a6eb7b26053cb4153e10e75f959ed0fe9
Praveen S Nair <praveen@it.regentdom>
2017-10-10 12:37:08 +0530

    Pages Normalised

---
+++ about.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<!-- Basic -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">	
+
+		<title>About Us Basic | Porto - Responsive HTML5 Template 5.7.2</title>	
+
+		<meta name="keywords" content="HTML5 Template" />
+		<meta name="description" content="Porto - Responsive HTML5 Template">
+		<meta name="author" content="okler.net">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+		<!-- Web Fonts  -->
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800%7CShadows+Into+Light" rel="stylesheet" type="text/css">
+
+		<!-- Vendor CSS -->
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="vendor/animate/animate.min.css">
+		<link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.min.css">
+		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">
+		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">
+		<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.min.css">
+
+		<!-- Theme CSS -->
+		<link rel="stylesheet" href="css/theme.css">
+		<link rel="stylesheet" href="css/theme-elements.css">
+		<link rel="stylesheet" href="css/theme-blog.css">
+		<link rel="stylesheet" href="css/theme-shop.css">
+
+		<!-- Skin CSS -->
+		<link rel="stylesheet" href="css/skins/default.css">
+
+		<!-- Theme Custom CSS -->
+		<link rel="stylesheet" href="css/custom.css">
+
+		<!-- Head Libs -->
+		<script src="vendor/modernizr/modernizr.min.js"></script>
+
+	</head>
+	<body>
+
+		<div class="body">
+
+			<!--  Header from header.html  -->
+			<?php include_once "header.html"; ?>
+
+			<div role="main" class="main">
+
+				<section class="page-header">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								<ul class="breadcrumb">
+									<li><a href="#">Home</a></li>
+									<li class="active">About Us</li>
+								</ul>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<h1>About Us</h1>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<div class="container">
+
+					<div class="row">
+						<div class="col-md-12">
+							<h2 class="word-rotator-title">
+								The New Way to <strong>
+									<span class="word-rotate" data-plugin-options="{'delay': 2000}">
+										<span class="word-rotate-items">
+											<span>success.</span>
+											<span>advance.</span>
+											<span>progress.</span>
+										</span>
+									</span>
+								</strong>
+							</h2>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-10">
+							<p class="lead">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum pellentesque imperdiet. Nulla lacinia iaculis nulla non <span class="alternative-font">metus.</span> pulvinar. Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut eu risus enim, ut pulvinar lectus. Sed hendrerit nibh.
+							</p>
+						</div>
+						<div class="col-md-2">
+							<a href="#" class="btn btn-lg btn-primary mt-xl">Join Our Team!</a>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-12">
+							<hr class="tall">
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-8">
+							<h3 class="heading-primary"><strong>Who</strong> We Are</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc <a href="#">vehicula</a> lacinia. Proin adipiscing porta tellus, ut feugiat nibh adipiscing sit amet. In eu justo a felis faucibus ornare vel id metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In eu libero ligula. Fusce eget metus lorem, ac viverra leo. Nullam convallis, arcu vel pellentesque sodales, nisi est varius diam, ac ultrices sem ante quis sem. Proin ultricies volutpat sapien, nec scelerisque ligula mollis lobortis.</p>
+							<p>Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc vehicula lacinia. Proin adipiscing porta tellus, ut feugiat nibh adipiscing <span class="alternative-font">metus</span> sit amet. In eu justo a felis faucibus ornare vel id metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In eu libero ligula. Fusce eget metus lorem, ac viverra leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In eu libero ligula. Fusce eget metus lorem, ac viverra leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In eu libero ligula.</p>
+						</div>
+						<div class="col-md-4">
+							<div class="featured-box featured-box-primary">
+								<div class="box-content">
+									<h4 class="text-uppercase">Behind the scenes</h4>
+									<ul class="thumbnail-gallery" data-plugin-lightbox data-plugin-options="{'delegate': 'a', 'type': 'image', 'gallery': {'enabled': true}}">
+										<li>
+											<a title="Benefits 1" href="img/benefits/benefits-1.jpg">
+												<span class="thumbnail mb-none">
+													<img src="img/benefits/benefits-1-thumb.jpg" alt="">
+												</span>
+											</a>
+										</li>
+										<li>
+											<a title="Benefits 2" href="img/benefits/benefits-2.jpg">
+												<span class="thumbnail mb-none">
+													<img src="img/benefits/benefits-2-thumb.jpg" alt="">
+												</span>
+											</a>
+										</li>
+										<li>
+											<a title="Benefits 3" href="img/benefits/benefits-3.jpg">
+												<span class="thumbnail mb-none">
+													<img src="img/benefits/benefits-3-thumb.jpg" alt="">
+												</span>
+											</a>
+										</li>
+										<li>
+											<a title="Benefits 4" href="img/benefits/benefits-4.jpg">
+												<span class="thumbnail mb-none">
+													<img src="img/benefits/benefits-4-thumb.jpg" alt="">
+												</span>
+											</a>
+										</li>
+										<li>
+											<a title="Benefits 5" href="img/benefits/benefits-5.jpg">
+												<span class="thumbnail mb-none">
+													<img src="img/benefits/benefits-5-thumb.jpg" alt="">
+												</span>
+											</a>
+										</li>
+										<li>
+											<a title="Benefits 6" href="img/benefits/benefits-6.jpg">
+												<span class="thumbnail mb-none">
+													<img src="img/benefits/benefits-6-thumb.jpg" alt="">
+												</span>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-12">
+							<hr class="tall">
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="heading-primary mt-xl">Our <strong>History</strong></h3>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-12">
+
+							<ul class="history">
+								<li class="appear-animation" data-appear-animation="fadeInUp">
+									<div class="thumb">
+										<img src="img/office/office-4.jpg" alt="" />
+									</div>
+									<div class="featured-box">
+										<div class="box-content">
+											<h4 class="heading-primary"><strong>2012</strong></h4>
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc vehicula lacinia. Proin adipiscing porta tellus, Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc vehicula lacinia. Proin adipiscing porta tellus,</p>
+										</div>
+									</div>
+								</li>
+								<li class="appear-animation" data-appear-animation="fadeInUp">
+									<div class="thumb">
+										<img src="img/office/office-3.jpg" alt="" />
+									</div>
+									<div class="featured-box">
+										<div class="box-content">
+											<h4 class="heading-primary"><strong>2010</strong></h4>
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc vehicula lacinia. Proin adipiscing porta tellus, Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc vehicula lacinia.</p>
+										</div>
+									</div>
+								</li>
+								<li class="appear-animation" data-appear-animation="fadeInUp">
+									<div class="thumb">
+										<img src="img/office/office-2.jpg" alt="" />
+									</div>
+									<div class="featured-box">
+										<div class="box-content">
+											<h4 class="heading-primary"><strong>2005</strong></h4>
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc vehicula lacinia. Proin adipiscing porta tellus, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc vehicula lacinia. Proin adipiscing porta tellus, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc vehicula lacinia. Proin adipiscing porta tellus,</p>
+										</div>
+									</div>
+								</li>
+								<li class="appear-animation" data-appear-animation="fadeInUp">
+									<div class="thumb">
+										<img src="img/office/office-1.jpg" alt="" />
+									</div>
+									<div class="featured-box">
+										<div class="box-content">
+											<h4 class="heading-primary"><strong>2000</strong></h4>
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc vehicula lacinia. Proin adipiscing porta tellus, Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc vehicula lacinia. Proin adipiscing porta tellus, Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc vehicula lacinia. Proin adipiscing porta tellus,</p>
+										</div>
+									</div>
+								</li>
+							</ul>
+
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+
+			<!-- Footer from footer.html -->
+			<?php include_once "footer.html"; ?>
+	
+		</div>
+
+		<!-- Vendor -->
+		<script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/jquery.appear/jquery.appear.min.js"></script>
+		<script src="vendor/jquery.easing/jquery.easing.min.js"></script>
+		<script src="vendor/jquery-cookie/jquery-cookie.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src="vendor/common/common.min.js"></script>
+		<script src="vendor/jquery.validation/jquery.validation.min.js"></script>
+		<script src="vendor/jquery.easy-pie-chart/jquery.easy-pie-chart.min.js"></script>
+		<script src="vendor/jquery.gmap/jquery.gmap.min.js"></script>
+		<script src="vendor/jquery.lazyload/jquery.lazyload.min.js"></script>
+		<script src="vendor/isotope/jquery.isotope.min.js"></script>
+		<script src="vendor/owl.carousel/owl.carousel.min.js"></script>
+		<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+		<script src="vendor/vide/vide.min.js"></script>
+		
+		<!-- Theme Base, Components and Settings -->
+		<script src="js/theme.js"></script>
+		
+		<!-- Theme Custom -->
+		<script src="js/custom.js"></script>
+		
+		<!-- Theme Initialization Files -->
+		<script src="js/theme.init.js"></script>
+
+		<!-- Google analytics -->
+		<?php include_once "g-analytics.html"; ?>
+
+	</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<!-- Basic -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">	
+
+		<title>Contact Us | Porto - Responsive HTML5 Template 5.7.2</title>	
+
+		<meta name="keywords" content="HTML5 Template" />
+		<meta name="description" content="Porto - Responsive HTML5 Template">
+		<meta name="author" content="okler.net">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+		<!-- Web Fonts  -->
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800%7CShadows+Into+Light" rel="stylesheet" type="text/css">
+
+		<!-- Vendor CSS -->
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="vendor/animate/animate.min.css">
+		<link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.min.css">
+		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">
+		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">
+		<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.min.css">
+
+		<!-- Theme CSS -->
+		<link rel="stylesheet" href="css/theme.css">
+		<link rel="stylesheet" href="css/theme-elements.css">
+		<link rel="stylesheet" href="css/theme-blog.css">
+		<link rel="stylesheet" href="css/theme-shop.css">
+
+		<!-- Skin CSS -->
+		<link rel="stylesheet" href="css/skins/default.css">
+
+		<!-- Theme Custom CSS -->
+		<link rel="stylesheet" href="css/custom.css">
+
+		<!-- Head Libs -->
+		<script src="vendor/modernizr/modernizr.min.js"></script>
+
+	</head>
+	<body>
+
+		<div class="body">
+
+			<!--  Header from header.html  -->
+			<?php include_once "header.html"; ?>
+
+			<div role="main" class="main">
+
+				<section class="page-header">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								<ul class="breadcrumb">
+									<li><a href="#">Home</a></li>
+									<li class="active">Contact Us</li>
+								</ul>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<h1>Contact Us</h1>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<!-- Google Maps - Go to the bottom of the page to change settings and map location. -->
+				<div id="googlemaps" class="google-map"></div>
+
+				<div class="container">
+
+					<div class="row">
+						<div class="col-md-6">
+
+							<div class="alert alert-success hidden mt-lg" id="contactSuccess">
+								<strong>Success!</strong> Your message has been sent to us.
+							</div>
+
+							<div class="alert alert-danger hidden mt-lg" id="contactError">
+								<strong>Error!</strong> There was an error sending your message.
+								<span class="font-size-xs mt-sm display-block" id="mailErrorMessage"></span>
+							</div>
+
+							<h2 class="mb-sm mt-sm"><strong>Contact</strong> Us</h2>
+							<form id="contactForm" action="php/contact-form.php" method="POST">
+								<div class="row">
+									<div class="form-group">
+										<div class="col-md-6">
+											<label>Your name *</label>
+											<input type="text" value="" data-msg-required="Please enter your name." maxlength="100" class="form-control" name="name" id="name" required>
+										</div>
+										<div class="col-md-6">
+											<label>Your email address *</label>
+											<input type="email" value="" data-msg-required="Please enter your email address." data-msg-email="Please enter a valid email address." maxlength="100" class="form-control" name="email" id="email" required>
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="form-group">
+										<div class="col-md-12">
+											<label>Subject</label>
+											<input type="text" value="" data-msg-required="Please enter the subject." maxlength="100" class="form-control" name="subject" id="subject" required>
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="form-group">
+										<div class="col-md-12">
+											<label>Message *</label>
+											<textarea maxlength="5000" data-msg-required="Please enter your message." rows="10" class="form-control" name="message" id="message" required></textarea>
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-md-12">
+										<input type="submit" value="Send Message" class="btn btn-primary btn-lg mb-xlg" data-loading-text="Loading...">
+									</div>
+								</div>
+							</form>
+						</div>
+						<div class="col-md-6">
+
+							<h4 class="heading-primary mt-lg">Get in <strong>Touch</strong></h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+							<hr>
+
+							<h4 class="heading-primary">The <strong>Office</strong></h4>
+							<ul class="list list-icons list-icons-style-3 mt-xlg">
+								<li><i class="fa fa-map-marker"></i> <strong>Address:</strong> 3rd Floor, Wing I # 2, Jyothirmaya InfoPark Phase - II, Kakkanad, Ernakulam, Kerala, India PIN - 682030.</li>
+ 								<li><i class="fa fa-phone"></i> <strong>Phone:</strong> +91 484 2982150.&nbsp;&nbsp;&nbsp; <a href="tel:+91 484 29821500">Call now?</a></li>
+								<li><i class="fa fa-envelope"></i> <strong>Email:</strong> info@regentglobal.org&nbsp;&nbsp;&nbsp; <a href="mailto:info@regentglobal.org" target="_blank">Send a mail?</a></li>
+							</ul>
+
+							<hr>
+
+							<h4 class="heading-primary">Business <strong>Hours</strong></h4>
+							<ul class="list list-icons list-dark mt-xlg">
+								<li><i class="fa fa-clock-o"></i> Monday - Friday - 9am to 11:30pm</li>
+								<li><i class="fa fa-clock-o"></i> Saturday - 9am to 4pm</li>
+								<li><i class="fa fa-clock-o"></i> Sunday - Closed</li>
+							</ul>
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+			<section class="call-to-action call-to-action-default with-button-arrow call-to-action-in-footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="call-to-action-content">
+								<h3>Porto is <strong>everything</strong> you need to create an <strong>awesome</strong> website!</h3>
+								<p>The <strong>#1 Selling</strong> HTML Site Template on ThemeForest</p>
+							</div>
+							<div class="call-to-action-btn">
+								<a href="http://themeforest.net/item/porto-responsive-html5-template/4106987" target="_blank" class="btn btn-lg btn-primary">Buy Now!</a><span class="arrow hlb hidden-xs hidden-sm hidden-md" data-appear-animation="rotateInUpLeft" style="top: -12px;"></span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<!-- Footer from footer.html -->
+			<?php include_once "footer.html"; ?>
+
+		</div>
+
+		<!-- Vendor -->
+		<script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/jquery.appear/jquery.appear.min.js"></script>
+		<script src="vendor/jquery.easing/jquery.easing.min.js"></script>
+		<script src="vendor/jquery-cookie/jquery-cookie.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src="vendor/common/common.min.js"></script>
+		<script src="vendor/jquery.validation/jquery.validation.min.js"></script>
+		<script src="vendor/jquery.easy-pie-chart/jquery.easy-pie-chart.min.js"></script>
+		<script src="vendor/jquery.gmap/jquery.gmap.min.js"></script>
+		<script src="vendor/jquery.lazyload/jquery.lazyload.min.js"></script>
+		<script src="vendor/isotope/jquery.isotope.min.js"></script>
+		<script src="vendor/owl.carousel/owl.carousel.min.js"></script>
+		<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+		<script src="vendor/vide/vide.min.js"></script>
+		
+		<!-- Theme Base, Components and Settings -->
+		<script src="js/theme.js"></script>
+
+		<!-- Current Page Vendor and Views -->
+		<script src="js/views/view.contact.js"></script>
+		
+		<!-- Theme Custom -->
+		<script src="js/custom.js"></script>
+		
+		<!-- Theme Initialization Files -->
+		<script src="js/theme.init.js"></script>
+
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC1GqTV7vUaFI-LyP0RkRVITKwoD0Hky3E"></script>
+		<script>
+
+			/*
+			Map Settings
+
+				Find the Latitude and Longitude of your address:
+					- http://universimmedia.pagesperso-orange.fr/geo/loc.htm
+					- http://www.findlatitudeandlongitude.com/find-address-from-latitude-and-longitude/
+
+			*/
+
+			// Map Markers
+			var mapMarkers = [{
+				address: "Jyothirmaya Infopark, Kakkanad",
+				html: "<strong>Corporate Office</strong><br>Kerala, India",
+				icon: {
+					image: "img/pin.png",
+					iconsize: [26, 46],
+					iconanchor: [12, 46]
+				},
+				popup: true
+			}];
+
+			// Map Initial Location
+			var initLatitude = 10.006926;
+			var initLongitude = 76.373405;
+
+			// Map Extended Settings
+			var mapSettings = {
+				controls: {
+					panControl: true,
+					zoomControl: true,
+					mapTypeControl: true,
+					scaleControl: true,
+					streetViewControl: true,
+					overviewMapControl: true
+				},
+				scrollwheel: false,
+				markers: mapMarkers,
+				latitude: initLatitude,
+				longitude: initLongitude,
+				zoom: 16
+			};
+
+			var map = $('#googlemaps').gMap(mapSettings);
+
+			// Map Center At
+			var mapCenterAt = function(options, e) {
+				e.preventDefault();
+				$('#googlemaps').gMap("centerAt", options);
+			}
+
+		</script>
+
+		<!-- Google analytics -->
+		<?php include_once "g-analytics.html"; ?>
+	
+	</body>
+</html>
